4ba96e4d1f602d857acfbad1df28052b4f51d346
Cristian <cristian.tirdad@gmail.com>
2026-07-15 17:51:22 +0200

    fix: update alignment in Home component for better layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import PdfTools from "@/components/PdfTools";
 
 export default function Home() {
   return (
-    <div className="flex flex-1 items-center justify-center bg-zinc-50 px-4 py-16 font-sans dark:bg-black">
+    <div className="flex flex-1 items-start justify-center bg-zinc-50 px-4 py-16 font-sans dark:bg-black">
       <main className="w-full">
         <PdfTools />
       </main>
